294333361bbb60a317ea0cc08eccc68d8499dc8b
JonyPlo <joonyyplo@gmail.com>
2024-08-07 15:10:24 -0300

    feat: :sparkles: add user model, routes and custom errors

---
+++ src/presentation/auth/routes.ts
@@ -0,0 +1,18 @@
+import { Router } from 'express'
+import { AuthController } from './controller'
+
+export class AuthRoutes {
+  static get routes(): Router {
+    const router = Router()
+
+    const controller = new AuthController()
+
+    // Definir las rutas
+    router.post('/register', controller.registerUser)
+    router.post('/login', controller.loginUser)
+
+    router.get('/validate-email/:token', controller.validateEmail)
+
+    return router
+  }
+}
