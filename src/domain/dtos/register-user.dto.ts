1eaa3a1c7b742ddeb1a624f2a35cc3512ab10ca7
JonyPlo <joonyyplo@gmail.com>
2024-08-07 16:13:47 -0300

    feat: :sparkles: add  register dto class

---
+++ src/domain/dtos/register-user.dto.ts
@@ -0,0 +1,20 @@
+import { regularExps } from '../../config'
+
+export class RegisterUserDto {
+  private constructor(
+    public name: string,
+    public email: string,
+    public password: string
+  ) {}
+
+  static create(object: { [key: string]: any }): [string?, RegisterUserDto?] {
+    const { name, email, password } = object
+    if (!name) return ['Name is required']
+    if (!email) return ['Email is required']
+    if (!regularExps.email.test(email)) return ['Email is not valid']
+    if (!password) return ['Password is required']
+    if (password.length < 6) return ['Password too short']
+
+    return [undefined, new RegisterUserDto(name, email, password)]
+  }
+}
