298391e4c9d48dff59859cda449158e401390ff6
JonyPlo <joonyyplo@gmail.com>
2024-08-07 16:00:57 -0300

    feat: :sparkles: add user entity

---
+++ src/domain/entities/user.entity.ts
@@ -0,0 +1,35 @@
+import { CustomError } from '../errors/custom.error'
+
+export class UserEntity {
+  constructor(
+    public id: string,
+    public name: string,
+    public email: string,
+    public emailValidated: boolean,
+    public password: string,
+    public role: string[],
+    public img?: string
+  ) {}
+
+  static fromObject(object: { [key: string]: any }): UserEntity {
+    const { id, _id, name, email, emailValidated, password, role, img } = object
+
+    if (!id && !_id) throw CustomError.badRequest('Id is required')
+    if (!name) throw CustomError.badRequest('Name is required')
+    if (!email) throw CustomError.badRequest('Email is required')
+    if (emailValidated === undefined)
+      throw CustomError.badRequest('EmailValidated is required')
+    if (!password) throw CustomError.badRequest('Password is required')
+    if (!role) throw CustomError.badRequest('Role is required')
+
+    return new UserEntity(
+      id || _id,
+      name,
+      email,
+      emailValidated,
+      password,
+      role,
+      img
+    )
+  }
+}
