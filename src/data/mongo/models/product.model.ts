c1ab0b788761112673690a4ac3c25714724a3c10
JonyPlo <joonyyplo@gmail.com>
2025-01-03 22:22:10 -0300

    feat: :sparkles: add two new models in mongodb
    category and product models added

---
+++ src/data/mongo/models/product.model.ts
@@ -0,0 +1,36 @@
+import { Schema, model } from 'mongoose'
+
+const productSchema = new Schema({
+  name: {
+    type: String,
+    required: [true, 'Name is required'],
+    unique: true,
+  },
+  available: {
+    type: Boolean,
+    default: true,
+  },
+  price: {
+    type: Number,
+    default: 0,
+  },
+  description: {
+    type: String,
+  },
+  image: {
+    type: String,
+  },
+  // Relations
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  category: {
+    type: Schema.Types.ObjectId,
+    ref: 'Category',
+    required: true,
+  },
+})
+
+export const ProductModel = model('Product', productSchema)
